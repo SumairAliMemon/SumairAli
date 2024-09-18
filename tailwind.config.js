88dac3fc021e568e0f827fa7518c1cccc8bf56b6
SumairAliMemon <sumairmemon897@gmail.com>
2024-09-19 04:46:42 +0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ './Code/*.html', 
+    './Code/*.css', 
+    './src/**/*.{js,ts,jsx,tsx}',
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
